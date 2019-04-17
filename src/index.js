6a294b328362eab9f92bec2a9a8aaf4c3ab32467
Krzysztof Piatkowski <kpi@trifork.com>
2019-04-17 11:57:14 +0200

    Started on skills

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "normalize.css";
 import "typeface-roboto-slab";
 import "ionicons/dist/scss/ionicons.scss";
 
-import "./scss/style.scss";
+import "./scss/index.scss";
 
 import "./index.html";
