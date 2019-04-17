1323419cb9acc51f0a9e87b1fe44de549ae74958
Krzysztof Piatkowski <kpi@trifork.com>
2019-04-17 10:59:13 +0200

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "normalize.css";
+import "typeface-roboto-slab";
+import "ionicons/dist/scss/ionicons.scss";
+
+import "./scss/style.scss";
+
+import "./index.html";
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          "file-loader?name=[name].[ext]",
+          "extract-loader",
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+              collapseWhitespace: true,
+              conservativeCollapse: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ],
+  resolve: {
+    extensions: [".js"]
+  },
+  devtool: "inline-source-map"
+};
